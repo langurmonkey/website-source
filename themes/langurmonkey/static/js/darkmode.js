5aa37174a564d5575edc90ca53ac0b418955dc71
langurmonkey <me@tonisagrista.com>
2023-07-10 10:47:43 +0200

    fix: theme toggle icon initialization.
    Former-commit-id: 838ecfce51294d221bf569fe624ad7aeda63cb7b

--- themes/langurmonkey/static/js/darkmode.js
+++ themes/langurmonkey/static/js/darkmode.js
@@ -1,34 +1,37 @@
 if(window.sessionStorage.getItem("dark-mode") == "true") {
-    // Toggle dark-mode class if "dark-mode" property of localStorage is true
-    darkModeAdd();
+  // Toggle dark-mode class if "dark-mode" property of localStorage is true
+  darkModeAdd();
 } else if (window.matchMedia && 
-    window.matchMedia('(prefers-color-scheme: dark)').matches && 
-    window.sessionStorage.getItem("dark-mode") == null) {
-    // Otherwise, check browser setting "prefers-color-scheme" is set to "dark"
-    darkModeAdd();
+  window.matchMedia('(prefers-color-scheme: dark)').matches && 
+  window.sessionStorage.getItem("dark-mode") == null) {
+  // Otherwise, check browser setting "prefers-color-scheme" is set to "dark"
+  darkModeAdd();
 }
 
-function lightModeAdd() {
-    document.body.classList.remove("dark-mode");
-    updateLightsButton(false);
-}
+// Adds the dark mode to the site, at startup.
 function darkModeAdd() {
-    document.body.classList.add("dark-mode");
-    updateLightsButton(true);
+  document.body.classList.add("dark-mode");
+  window.onload = function () {
+    updateThemeToggleButton(true);
+  }
 }
+
+// Toggles dark and light modes.
 function darkModeToggle() {
-    // Add classes
-    document.body.classList.toggle("dark-mode");
-    is_dark = document.body.classList.contains("dark-mode");
-    window.sessionStorage.setItem("dark-mode", is_dark);
-    updateLightsButton(is_dark);
+  // Add classes
+  document.body.classList.toggle("dark-mode");
+  is_dark = document.body.classList.contains("dark-mode");
+  window.sessionStorage.setItem("dark-mode", is_dark);
+  updateThemeToggleButton(is_dark);
 }
-function updateLightsButton(is_dark) {
-    var toggle = document.getElementById("toggle-theme");
-    toggle.classList.remove("fa-sun-o", "fa-moon");
-    if(is_dark) {
-       toggle.classList.add("fa-sun-o"); 
-    } else {
-       toggle.classList.add("fa-moon"); 
-    }
+
+// Updates the theme toggle button icon.
+function updateThemeToggleButton(is_dark) {
+  var toggle = document.getElementById("toggle-theme");
+  toggle.classList.remove("fa-sun-o", "fa-moon");
+  if(is_dark) {
+    toggle.classList.add("fa-sun-o"); 
+  } else {
+    toggle.classList.add("fa-moon"); 
+  }
 }
